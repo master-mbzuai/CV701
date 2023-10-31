5b8a67cbbf856f6aa7aaada90fa54a2d0945b361
sebastian <sebastian.cavada.lab@gmail.com>
2023-10-31 18:00:17 +0400

    small changes on the folders

--- 03/analysis/plot2.py
+++ 03/analysis/plot2.py
@@ -1,6 +1,6 @@
 
 import re
-from matplotlib import pyplot as pltplot
+from matplotlib import pyplot as plt
 
 def parsefile(log_text):
 
@@ -22,31 +22,57 @@ path = "../code/task1.1/"
 #text_file = open("../logs/1000_log.txt", "r")
 
 
+text_file = open(path + "adamW.txt", "r")
+input_string = text_file.read()
+
+adamW_t, adamW_v = parsefile(input_string)
+
+adamW_t = [x[0]/64 for x in adamW_t]
+adamW_v = [x[0] for x in adamW_v]
+epochs = [x for x in range(len(adamW_t))]
+
+#print(adamW_t)
+
 text_file = open(path + "adam.txt", "r")
 input_string = text_file.read()
 
 adam_t, adam_v = parsefile(input_string)
 
-# text_file = open(path + "adamW.txt", "r")
-# input_string = text_file.read()
+adam_t = [x[0] for x in adam_t]
+adam_v = [x[0] for x in adam_v]
 
-# adamW = parsefile(input_string)
+text_file = open(path + "SGD.txt", "r")
+input_string = text_file.read()
 
-# text_file = open(path + "SGD.txt", "r")
-# input_string = text_file.read()
+SGD_t, SGD_v = parsefile(input_string)
 
-# SGD = parsefile(input_string)
+SGD_t = [x[0]/64 for x in SGD_t]
+SGD_v = [x[0] for x in SGD_v]
 
-adam_t = [x[0]/64 for x in adam_t]
-epochs = [x for x in range(len(adam_t))]
+# Create a figure
+plt.figure(figsize=(18, 6))
 
-print(adam_t)
+# First subplot
+plt.subplot(1, 3, 1)
+plt.plot(epochs, SGD_t, alpha=0.7, c='blue', label='Parameters')
+plt.plot(epochs, SGD_v, alpha=0.7, c='red', label='Parameters')
+plt.grid(True)
+plt.title('Train loss (blue) VS Validation loss - SGD ')
+
+# Second subplot
+plt.subplot(1, 3, 3)
+plt.plot(epochs, adamW_t, alpha=0.7, c='blue', label='Parameters')
+plt.plot(epochs, adamW_v, alpha=0.7, c='red', label='Parameters')
+plt.grid(True)
+plt.title('Train loss (blue) VS Validation loss - ADAMW ')
+
+plt.subplot(1, 3, 2)
 
 # Create the scatter plot
-plt.plot(adam_t, alpha=0.7, c='blue', label='Parameters')
-plt.plot(adam_v, alpha=0.7, c='red', label='Parameters')
+plt.plot(epochs, adam_t, alpha=0.7, c='blue', label='Parameters')
+plt.plot(epochs, adam_v, alpha=0.7, c='red', label='Parameters')
 
-plt.title('Train loss (blue) VS Validation loss')
+plt.title('Train loss (blue) VS Validation loss - ADAM')
 plt.xlabel('Epoch')
 plt.ylabel('Loss')
 plt.grid(True)
