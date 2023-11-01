ffc619059097ec79d29857d4e3c42d8bb62d40f4
sebastian <sebastian.cavada.lab@gmail.com>
2023-11-01 11:15:37 +0400

    some refactoring

---
+++ 03/code/task1.1/run_exp.sh
@@ -0,0 +1,14 @@
+
+# Define the lists
+lr=("0.001" "0.0001" "0.00001")
+opt=("adam" "adamW" "SGD")
+batch_size=("64" "128")
+
+# Iterate through all combinations
+for learning_rate in "${lr[@]}"; do
+  for optimizer in "${opt[@]}"; do
+    for size in "${batch_size[@]}"; do              
+        xterm -e "python main.py --lr $learning_rate --opt $optimizer --batch_size $size --epochs 2; exit" &      
+    done
+  done
+done
