5aead4863f5c5ee0bf9adb4f0d180ff3942b5b44
sebastian <sebastian.cavada.lab@gmail.com>
2023-11-28 17:56:14 +0400

    int8 added

--- 04/code/task1/convert.py
+++ 04/code/task1/convert.py
@@ -3,6 +3,8 @@ import torch
 
 from models.model1_pre import FacialPoints
 from micromind.utils.parse import parse_arguments
+from micromind import convert
+
 
 if __name__ == "__main__":  
 
@@ -11,3 +13,5 @@ if __name__ == "__main__":
     m = FacialPoints(hparams) 
 
     m.export("model.tflite", "tflite", (3, 224, 224))
+
+    #convert.convert_to_tflite(m, "tflite", batch_quant=torch.randn(224,224,3))
